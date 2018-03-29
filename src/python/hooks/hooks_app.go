1055040f3f85bb785be98a6dfb64f543f57e6456
Tyler Phelan <tphelan@pivotal.io>
2018-03-29 12:28:10 -0400

    wip; try using bash for a tastier shell session

--- src/python/hooks/hooks_app.go
+++ src/python/hooks/hooks_app.go
@@ -25,7 +25,7 @@ func (h AppHook) BeforeCompile(compiler *libbuildpack.Stager) error {
 		if err := os.Chmod(path, 0755); err != nil {
 			return err
 		}
-		cmd := exec.Command("/bin/sh", path)
+		cmd := exec.Command("/usr/bin/env", "bash", path)
 		cmd.Dir = compiler.BuildDir()
 		output, err := cmd.Output()
 		if err != nil {
